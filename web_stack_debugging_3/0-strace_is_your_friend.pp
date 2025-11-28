0e4c7e42988e90375561c893f77ff0e848546e28
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2025-11-28 09:49:11 +0000

    Debug using strace and puppet

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -3,24 +3,24 @@
 #  - ensures web files are owned by www-data
 #  - sets directory permissions to 755 and file permissions to 644
 #  - ensures apache2 service is enabled and running
-# Ensure required PHP extensions are install
+#  - installs required PHP extensions
 
 exec { 'chown-www-data':
-  command   => '/bin/chown -R www-data:www-data /var/www/html',
-  onlyif    => 'test "$(stat -c %U /var/www/html 2>/dev/null)" != "www-data"',
-  path      => ['/bin', '/usr/bin'],
+  command  => '/bin/chown -R www-data:www-data /var/www/html',
+  onlyif   => 'test "$(stat -c %U /var/www/html 2>/dev/null)" != "www-data"',
+  path     => ['/bin', '/usr/bin'],
 }
 
 exec { 'fix-permissions':
-  command   => '/bin/bash -c "find /var/www/html -type d -exec chmod 0755 {} \\; && find /var/www/html -type f -exec chmod 0644 {} \\;"',
-  onlyif    => 'test -d /var/www/html',
-  path      => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-  require   => Exec['chown-www-data'],
+  command  => '/bin/bash -c "find /var/www/html -type d -exec chmod 0755 {} \\; && find /var/www/html -type f -exec chmod 0644 {} \\;"',
+  onlyif   => 'test -d /var/www/html',
+  path     => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
+  require  => Exec['chown-www-data'],
 }
 
 service { 'apache2':
-  ensure    => running,
-  enable    => true,
+  ensure   => running,
+  enable   => true,
   subscribe => [Exec['chown-www-data'], Exec['fix-permissions']],
 }
 
