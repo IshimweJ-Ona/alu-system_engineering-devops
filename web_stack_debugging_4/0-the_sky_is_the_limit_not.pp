f01ccdb78796eee5bf497e0bb40402f68fe80a54
IshimweJ-Ona <j.ishimwe4@alustudent.com>
2025-11-30 06:20:25 +0000

    Corrected commands

--- web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,10 +1,14 @@
-# This manifest fixes Nginx under heavy load by disabling sendfile and increasing worker connections
+# This manifest fixes Nginx to handle high traffic with zero failed requests
 
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/sendfile on;/sendfile off;/" /etc/nginx/nginx.conf &&
-              sed -i "s/worker_connections [0-9]*/worker_connections 4096/" /etc/nginx/nginx.conf &&
-              sed -i "s/worker_processes [0-9]*/worker_processes 4/" /etc/nginx/nginx.conf &&
-              service nginx restart',
-  path    => ['/bin', '/usr/bin'],
+exec { 'set nginx ulimit':
+  command => 'grep -q "^ULIMIT=10000" /etc/default/nginx || echo "ULIMIT=10000" >> /etc/default/nginx',
+  path    => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
+}
+
+exec { 'restart nginx':
+  command => 'service nginx restart',
+  refreshonly => true,
+  subscribe   => Exec['set nginx ulimit'],
+  path        => '/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin',
 }
 
