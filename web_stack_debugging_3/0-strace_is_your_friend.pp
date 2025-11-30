82f55a6f0eb89860d636f48d4d4bd478e8aacf4c
ISHIMWE Jonathan <j.ishimwe4@alustudent.com>
2025-11-30 08:29:20 +0200

    Fix 'phpp' extensions in wp-settings.php

--- web_stack_debugging_3/0-strace_is_your_friend.pp
+++ web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,21 +1,7 @@
-# Ensure Apache can read and serve the web files by fixing ownership and permissions for /var/www/html
+# Fixes bad 'phpp' extensions to 'php' in the WordPress file 'wp-settings.php'
 
-exec { 'chown-www-data':
-   command => '/bin/chown -R www-data:www-data /var/www/html',
-   onlyif  => 'test "$(stat -c %U /var/www/html 2>/dev/null)" != "www-data"',
-   path    => ['/bin', '/usr/bin'],
+exec { 'fix-wordpress':
+  command => 'sed -i "s/phpp/php/g" /var/www/html/wp-settings.php',
+  path    => ['/usr/local/bin', '/usr/bin', '/bin'],
+  onlyif  => 'grep -q phpp /var/www/html/wp-settings.php',
 }
-
-exec { 'fix-permissions':
-   command => '/bin/bash -c "find /var/www/html -type d -exec chmod 0755 {} \\; && find /var/www/html -type f -exec chmod 0644 {} \\;"',
-   onlyif  => 'test -d /var/www/html',
-   path    => ['/bin', '/usr/bin', '/sbin', '/usr/sbin'],
-   require => Exec['chown-www-data'],
-}
-
-service { 'apache2':
-   ensure   => running,
-   enable   => true,
-   subscribe=> [Exec['chown-www-data'], Exec['fix-permissions']],
-}
-
